61ffc4524ea54e5c9dc68df188f0dec9615b1315
Joshua Jadulco <joshuajadulco@gmail.com>
2025-10-15 02:37:43 -0400

    feat: Add fillable attributes and casting to FormattedLog model
    Defines fillable fields for raw_log and formatted_log attributes
    Adds array casting for formatted_log to automatically handle serialization

--- app/Models/FormattedLog.php
+++ app/Models/FormattedLog.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormattedLog extends Model
 {
-    //
+    protected $fillable = [
+        'raw_log',
+        'formatted_log',
+    ];
+
+    protected $casts = [
+        'formatted_log' => 'array',
+    ];
 }
